ac11e62a66fd02ff934c635c4faef1cfe19b9eab
hamraskannur <kkhamras741@gmail.com>
2023-11-05 23:59:47 +0530

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import Download from "../../components/Download/Download"
 
 const Home = () => {
   return (
       <div>Home</div>
-    );
+
+  );
 };
 
 export default Home;
