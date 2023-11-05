30b4d2f0b85f28093375c6a1bced43813baba9b5
hamraskannur <kkhamras741@gmail.com>
2023-11-05 22:22:05 +0530

    new

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import img from "../../assets/Mobile-work_26-removebg.png"
+import playStoreIcon from "../../assets/play-store-icon.png"
+import AppStoreIcon from "../../assets/App-Store-icon.png"
 
 const Download = () => {
   return (
@@ -8,11 +10,13 @@ const Download = () => {
         <div className=" grid gap-4 ">
           <h1 className="text-4xl   max"><strong>Download our mobile <br/> application.</strong></h1>
           <p className="text-[#333e48] leading-7 ">
-            <strong>Download Appwind mobile banking app for IOS <br className="md:block hidden"/>& Android to
+            <strong>Download Appwind mobile banking app for IOS & <br className="md:block hidden"/>Android to
             manage your online money.</strong>
           </p>
-          <div className="flex">
-          <button className="bg-[#00b7aa] p-3 pl-4 rounded-md text-[#f6f5f8]"><strong>Play Store</strong></button>
+          <div className="flex gap-2">
+          <button className="bg-[#00b7aa] p-2 py-3 px-4 rounded-md text-[#f6f5f8] flex"><img className="w-8 h-7 text" src={playStoreIcon} alt="" /><strong >Play Store</strong></button>
+          <button className="bg-[#333e48]  p-2 py-3 px-4 rounded-md text-[#f6f5f8] flex"><img className="w-7 h-6  text" src={AppStoreIcon} alt="" /><strong className="pl-1">App Store</strong></button>
+
           </div>
         </div>
       </div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import Download from "../../components/Download/Download"
 
 const Home = () => {
   return (
     <>
     <div className='md:container'>
     <div>Home</div>
+    
+
     </div>
     </>
   )
