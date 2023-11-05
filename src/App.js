85c38b671a588d27e35915c41cfab804d376c1b2
shibil <shibi2772@gmail.com>
2023-11-05 20:31:52 +0530

    shibil:updates

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Navbar from './components/navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-       <h1>HELLO</h1>
+      <Navbar/>
+       <h1 className='text-primary'>HELLO</h1>
     </div>
   );
 }
