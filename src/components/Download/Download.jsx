462d7f71e2c874f97bb1e44da403bfbf716049fd
hamraskannur <kkhamras741@gmail.com>
2023-11-05 21:52:57 +0530

    Download component 80% completed

--- src/components/Download/Download.jsx
+++ src/components/Download/Download.jsx
@@ -1,20 +1,22 @@
 import React from "react";
+import img from "../../assets/Mobile-work_26-removebg.png"
 
 const Download = () => {
   return (
-    <div className="flex w-full mt-5 bg-[#f6f5f8] rounded-2xl">
-      <div className="p-10 w-1/2  ">
-        <div className="mx-auto  grid gap-4 ">
-          <h1 className="text-4xl  max"><strong>Download our mobile <br/> application.</strong></h1>
-          <p className="text-[#333e48]  ">
-            Download Appwind mobile banking app for IOS <br />& Android to
-            manage your online money.
+    <div className="md:flex w-full mt-5 bg-[#f6f5f8] rounded-2xl  ">
+      <div className="md:w-1/2 w-full my-auto flex pb-0  p-8 justify-center">
+        <div className=" grid gap-4 ">
+          <h1 className="text-4xl   max"><strong>Download our mobile <br/> application.</strong></h1>
+          <p className="text-[#333e48] leading-7 ">
+            <strong>Download Appwind mobile banking app for IOS <br className="md:block hidden"/>& Android to
+            manage your online money.</strong>
           </p>
-          <button className=""></button>
-
+          <div className="flex">
+          <button className="bg-[#00b7aa] p-3 pl-4 rounded-md text-[#f6f5f8]"><strong>Play Store</strong></button>
+          </div>
         </div>
       </div>
-      <div className=" w-1/2">image</div>
+      <div className=" ml-auto"><img className="w-full rounded-br-2xl  h-96" src={img} /></div>
     </div>
   );
 };
@@ -22,7 +24,3 @@ const Download = () => {
 export default Download;
 
 
-// <Download/>
-// <div className='bg-[#00b7aa] w-full h-10 mt-52'></div>
-// <div className='bg-[#f6f5f8] w-full h-10'></div>
-// <div className='bg-[#333e48] w-full h-10'></div>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,14 @@ import React from 'react'
 
 const Home = () => {
   return (
+    <>
+    <div className='md:container'>
     <div>Home</div>
+    </div>
+    </>
   )
 }
 
 export default Home
+
+
