ac87a6443f7a9b00a2cc764902188d5905dfe600
shibil <shibi2772@gmail.com>
2023-11-05 23:58:42 +0530

    shibil:layout updates

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import Layout from '../layout/Layout';
+import Download from '../../components/Download/Download';
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <Layout>
+      <Download/>
+    </Layout>
+    </>
   )
 }
 
