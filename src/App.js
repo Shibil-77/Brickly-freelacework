30ff0e536e597f28f9b69209c91d693d573e561f
hamraskannur <kkhamras741@gmail.com>
2023-11-05 20:26:35 +0530

    tailwind config reset

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Download from './components/Download/Download';
 
 function App() {
   return (
     <div className="App">
-       <h1>HELLO</h1>
+       <h1 >HELLO</h1>
     </div>
   );
 }
---
+++ src/components/Download/Download.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Download = () => {
+  return (
+    <div className='flex w-full border-2' >
+       <div>
+          <h1>Download Now!</h1>
+          <h1>Download our mobile application.</h1>
+          <p>Download Appwind mobile banking app for IOS & Android to manage your online money.</p>
+       </div>
+       <div>
+         image
+       </div>
+    </div>
+  )
+}
+
+export default Download
