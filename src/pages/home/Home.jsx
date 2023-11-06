c99c76e668ca0caf259a6fe0b6f99f49cae31938
shibil <shibi2772@gmail.com>
2023-11-06 13:42:57 +0530

    shibil:works completed

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,10 +1,19 @@
-
-import React from 'react'
+import React from "react";
+import Layout from "../layout/Layout";
+import Download from '../../components/Download/Download'
+import '../../App.css'
+import Works from "../../components/works/Works";
 
 const Home = () => {
   return (
-    <div>Home</div>
-  )
-}
+    <Layout>
+  
+      <Download/>
+      <Works/>
+      <div className="bg " >Home</div>
 
-export default Home
+    </Layout>
+  );
+};
+
+export default Home;
