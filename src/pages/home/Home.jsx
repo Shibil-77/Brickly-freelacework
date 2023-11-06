f40aacfe3a6e6ead1e78621c7eaad331f3bdb1aa
hamraskannur <kkhamras741@gmail.com>
2023-11-06 13:10:42 +0530

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,6 +2,7 @@
 import React from 'react'
 import Layout from '../layout/Layout';
 import Download from '../../components/Download/Download';
+import Content from '../../components/content/Content';
 
 const Home = () => {
   return (
