a804e64c694fb71d83be2a63532b866216b9831e
hamraskannur <kkhamras741@gmail.com>
2023-11-06 21:17:13 +0530

    new

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,19 +1,20 @@
 import React from "react";
 import Layout from "../layout/Layout";
-import Download from '../../components/Download/Download'
-import '../../App.css'
+import Download from "../../components/Download/Download";
+import "../../App.css";
 import Works from "../../components/works/Works";
+import Review from "../../components/review/Review";
+import Content from "../../components/content/Content";
 
 const Home = () => {
   return (
     <Layout>
-  
-      <Download/>
-      <Works/>
-      <div className="bg " >Home</div>
-
+      <Works />
+      <Download />
+      <Content/>
+      <Review />
     </Layout>
   );
 };
 
-export default Home
+export default Home;
