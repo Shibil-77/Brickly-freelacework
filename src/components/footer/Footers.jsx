9698ea4615b851606fa1d24fc4e33f8b9e6a002b
hamraskannur <kkhamras741@gmail.com>
2023-11-20 15:30:54 +0530

    new

--- src/components/footer/Footers.jsx
+++ src/components/footer/Footers.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import logo from '../../assets/brickly-01.png'
-
+import logo from "../../assets/brickly-01.png";
 
 const Footers = () => {
   return (
@@ -23,7 +22,15 @@ const Footers = () => {
                   />
                 </a>
                 <p className="mb-7 text-base text-body-color dark:text-dark-6">
-                <strong>Company location :</strong> PM Arcade Melmuri, PO, Alathurpedi, Malappuram, Kerala 676517<br/>Support @brickly. Ai
+                  <strong>Company location :</strong> PM Arcade Melmuri, PO,
+                  Alathurpedi, Malappuram, Kerala 676517
+                  <br />
+                  Support @brickly. Ai
+                </p>
+
+                <p className="mb-7 text-base text-body-color dark:text-dark-6">
+                  DataHQ
+                  Q1 Mall, Palarivattom, Kochi, Kerala 682028
                 </p>
                 <p className="flex items-center text-sm font-medium text-dark dark:text-white">
                   <span className="mr-3 text-text-color">
@@ -126,21 +133,18 @@ const Footers = () => {
                   </a>
                   <a
                     href="https://instagram.com/pmr_infos?igshid=MWFtZ3hmMGV1eDR0dA=="
-                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-texttext-text-color hover:bg-text-color hover:text-white dark:border-dark-3 dark:text-white dark:hover:border-texttext-text-color sm:mr-4 lg:mr-3 xl:mr-4"
+                    className="mr-3 flex h-8 w-8 items-center justify-center rounded-full border border-stroke text-dark hover:border-texttext-text-color  dark:border-dark-3 dark:text-white dark:hover:border-texttext-text-color sm:mr-4 lg:mr-3 xl:mr-4"
                   >
-                    <svg
-                      width="14"
-                      height="14"
-                      viewBox="0 0 14 14"
-                      className="fill-current"
-                    >
-                      <path d="M13.0214 0H1.02084C0.453707 0 0 0.451613 0 1.01613V12.9839C0 13.5258 0.453707 14 1.02084 14H12.976C13.5432 14 13.9969 13.5484 13.9969 12.9839V0.993548C14.0422 0.451613 13.5885 0 13.0214 0ZM4.15142 11.9H2.08705V5.23871H4.15142V11.9ZM3.10789 4.3129C2.42733 4.3129 1.90557 3.77097 1.90557 3.11613C1.90557 2.46129 2.45002 1.91935 3.10789 1.91935C3.76577 1.91935 4.31022 2.46129 4.31022 3.11613C4.31022 3.77097 3.81114 4.3129 3.10789 4.3129ZM11.9779 11.9H9.9135V8.67097C9.9135 7.90323 9.89082 6.8871 8.82461 6.8871C7.73571 6.8871 7.57691 7.74516 7.57691 8.60323V11.9H5.51254V5.23871H7.53154V6.16452H7.55423C7.84914 5.62258 8.50701 5.08065 9.52785 5.08065C11.6376 5.08065 12.0232 6.43548 12.0232 8.2871V11.9H11.9779Z" />
-                    </svg>
+                    <img
+                      className="w-5 h-5"
+                      src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQpAlIHViSx0vpXvEx9I5NLTyah_22dZwDpsw&usqp=CAU"
+                      alt=""
+                    />
                   </a>
                 </div>
-                <p className="text-base text-body-color dark:text-dark-6">
+                {/* <p className="text-base text-body-color dark:text-dark-6">
                   &copy; 2025 TailGrids
-                </p>
+                </p> */}
               </div>
             </div>
           </div>
@@ -209,29 +213,29 @@ const Footers = () => {
 export default Footers;
 
 const LinkGroup = ({ children, header }) => {
-    return (
-      <>
-        <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
-          <div className="mb-10 w-full">
-            <h4 className="mb-9 text-lg font-semibold text-dark dark:text-white">
-              {header}
-            </h4>
-            <ul className="space-y-3">{children}</ul>
-          </div>
+  return (
+    <>
+      <div className="w-full px-4 sm:w-1/2 lg:w-2/12">
+        <div className="mb-10 w-full">
+          <h4 className="mb-9 text-lg font-semibold text-dark dark:text-white">
+            {header}
+          </h4>
+          <ul className="space-y-3">{children}</ul>
         </div>
-      </>
-    );
-  };
-  
+      </div>
+    </>
+  );
+};
+
 const NavLink = ({ link, label }) => {
-    return (
-      <li>
-        <a
-          href={link}
-          className="inline-block text-base leading-loose text-body-color hover:text-text-color dark:text-dark-6"
-        >
-          {label}
-        </a>
-      </li>
-    );
+  return (
+    <li>
+      <a
+        href={link}
+        className="inline-block text-base leading-loose text-body-color hover:text-text-color dark:text-dark-6"
+      >
+        {label}
+      </a>
+    </li>
+  );
 };
